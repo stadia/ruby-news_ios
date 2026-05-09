e8e83b0223aac14612467ff0f2a5d6f9f2532283
jeff.dean <stadia@gmail.com>
2026-05-09 22:39:17 +0900

    revert: ruby_newsApp.swift 원상 복구
    핀치 줌 차단이 서버 뷰포트 태그로 이동했으므로
    이전에 추가했던 configureHotwire() 및 불필요한 import 제거

--- ruby-news/ruby_newsApp.swift
+++ ruby-news/ruby_newsApp.swift
@@ -5,27 +5,12 @@
 //  Created by JEFF.DEAN on 5/6/26.
 //
 
-import HotwireNative
 import SwiftUI
-import WebKit
 
 @main
 struct ruby_newsApp: App {
     init() {
         WebSessionBridge().restorePersistedCookiesToSharedStorage()
-        configureHotwire()
-    }
-
-    private func configureHotwire() {
-        Hotwire.config.makeCustomWebView = { configuration in
-            let webView = WKWebView(frame: .zero, configuration: configuration)
-            #if DEBUG
-            if #available(iOS 16.4, *) {
-                webView.isInspectable = true
-            }
-            #endif
-            return webView
-        }
     }
 
     var body: some Scene {
