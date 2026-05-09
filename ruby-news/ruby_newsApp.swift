0cc1a3c30522fe5d9a1a7b16b0f5ab5cbae6b62f
jeff.dean <stadia@gmail.com>
2026-05-09 18:04:25 +0900

    fix: 웹뷰 핀치 줌 비활성화
    Hotwire.config.makeCustomWebView에서 scrollView.pinchGestureRecognizer를 비활성화.
    모든 Hotwire 화면(피드, 기사, 프로필 등)에 일괄 적용.
    DEBUG 빌드의 isInspectable 동작은 그대로 유지.

--- ruby-news/ruby_newsApp.swift
+++ ruby-news/ruby_newsApp.swift
@@ -5,12 +5,28 @@
 //  Created by JEFF.DEAN on 5/6/26.
 //
 
+import HotwireNative
 import SwiftUI
+import WebKit
 
 @main
 struct ruby_newsApp: App {
     init() {
         WebSessionBridge().restorePersistedCookiesToSharedStorage()
+        configureHotwire()
+    }
+
+    private func configureHotwire() {
+        Hotwire.config.makeCustomWebView = { configuration in
+            let webView = WKWebView(frame: .zero, configuration: configuration)
+            #if DEBUG
+            if #available(iOS 16.4, *) {
+                webView.isInspectable = true
+            }
+            #endif
+            webView.scrollView.pinchGestureRecognizer?.isEnabled = false
+            return webView
+        }
     }
 
     var body: some Scene {
